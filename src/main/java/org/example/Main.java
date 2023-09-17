d44c6d225605a2b1827d7b082db2f582e163be03
alissonspada <alisson_spada@hotmail.com>
2023-09-16 21:14:47 -0300

    media de itens

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.model.Inventory;
+import org.example.model.Item;
+import org.example.repositories.InventoryRepository;
+import org.example.usecase.DisplayAveragesUseCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-        
     }
 }
