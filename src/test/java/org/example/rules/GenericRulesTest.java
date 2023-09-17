a602a97441b5f976cbcfcd995fb15a00c1317ad5
neo <matheusventureli50@gmail.com>
2023-09-16 22:01:00 -0300

    RegistrationRules / RegistrationUseCaseTest

--- src/test/java/org/example/rules/GenericRulesTest.java
+++ src/test/java/org/example/rules/GenericRulesTest.java
@@ -1,7 +1,14 @@
 package org.example.rules;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenericRulesTest {
+
+    @Test
+    void should() {
+
+    }
 
 }
